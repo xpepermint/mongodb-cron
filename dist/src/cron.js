0a07256be5991867433a7805ed7eacd6f4c18ab0
Elton Borssoi <elton.borssoi@instant.co>
2018-02-28 22:49:17 -0500

    Fix rescheduling and added features
    Added an option to pass the original document to onDocument
    Added an option to not run the job for the first time if sleepUntil is null
    Added an option to save job statistics to a collection
    Fixed a bug that on getNextStart that skipped a reschedule
    Updated the documentation to match the new features
    Bumped the package version
    Added more tests

--- dist/src/cron.js
+++ dist/src/cron.js
@@ -46,6 +46,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var later = require("later");
 var dot = require("dot-object");
 var moment = require("moment");
+var _ = require("lodash");
+var os = require("os");
 var mongodb_1 = require("mongodb");
 var es6_sleep_1 = require("es6-sleep");
 var MongoCron = (function () {
@@ -53,7 +55,8 @@ var MongoCron = (function () {
         this.running = false;
         this.processing = false;
         this.idle = false;
-        this.config = __assign({ onDocument: console.log, onError: console.error, nextDelay: 0, reprocessDelay: 0, idleDelay: 0, lockDuration: 600000, sleepUntilFieldPath: 'sleepUntil', intervalFieldPath: 'interval', repeatUntilFieldPath: 'repeatUntil', autoRemoveFieldPath: 'autoRemove' }, config);
+        this.config = __assign({ statisticsCollection: null, onDocument: console.log, onError: console.error, nextDelay: 0, reprocessDelay: 0, idleDelay: 0, lockDuration: 600000, rescheduleIfSleepUntilIsNull: false, returnOriginalDocument: false, sleepUntilFieldPath: 'sleepUntil', intervalFieldPath: 'interval', repeatUntilFieldPath: 'repeatUntil', autoRemoveFieldPath: 'autoRemove', cronName: 'mongodb-cron' }, config);
+        this.serverName = os.hostname();
     }
     MongoCron.prototype.isRunning = function () {
         return this.running;
@@ -109,24 +112,24 @@ var MongoCron = (function () {
     MongoCron.prototype.tick = function () {
         return __awaiter(this, void 0, void 0, function () {
             var _this = this;
-            var doc, err_1;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
+            var _a, doc, lockedSleepUntil, clonedDoc, jobStart, err_1;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
                     case 0:
                         if (!this.running)
                             return [2];
                         return [4, es6_sleep_1.promise(this.config.nextDelay)];
                     case 1:
-                        _a.sent();
+                        _b.sent();
                         if (!this.running)
                             return [2];
                         this.processing = true;
-                        _a.label = 2;
+                        _b.label = 2;
                     case 2:
-                        _a.trys.push([2, 12, , 14]);
+                        _b.trys.push([2, 12, , 14]);
                         return [4, this.lockNext()];
                     case 3:
-                        doc = _a.sent();
+                        _a = _b.sent(), doc = _a[0], lockedSleepUntil = _a[1];
                         if (!!doc) return [3, 7];
                         this.processing = false;
                         if (!!this.idle) return [3, 5];
@@ -134,30 +137,42 @@ var MongoCron = (function () {
                         if (!this.config.onIdle) return [3, 5];
                         return [4, this.config.onIdle.call(this, this)];
                     case 4:
-                        _a.sent();
-                        _a.label = 5;
+                        _b.sent();
+                        _b.label = 5;
                     case 5: return [4, es6_sleep_1.promise(this.config.idleDelay)];
                     case 6:
-                        _a.sent();
+                        _b.sent();
                         return [3, 11];
                     case 7:
                         this.idle = false;
                         if (!this.config.onDocument) return [3, 9];
-                        return [4, this.config.onDocument.call(this, doc, this)];
+                        if (!!(this.config.rescheduleIfSleepUntilIsNull
+                            && dot.pick(this.config.sleepUntilFieldPath, doc) === null
+                            && dot.pick(this.config.intervalFieldPath, doc))) return [3, 9];
+                        clonedDoc = _.cloneDeep(doc);
+                        if (!this.config.returnOriginalDocument) {
+                            dot.del(this.config.sleepUntilFieldPath, clonedDoc);
+                            dot.str(this.config.sleepUntilFieldPath, lockedSleepUntil, clonedDoc);
+                        }
+                        jobStart = moment.utc().toDate();
+                        return [4, this.config.onDocument.call(this, clonedDoc, this)];
                     case 8:
-                        _a.sent();
-                        _a.label = 9;
+                        _b.sent();
+                        if (this.config.statisticsCollection) {
+                            this.saveStatistics(doc, jobStart, moment.utc().toDate());
+                        }
+                        _b.label = 9;
                     case 9: return [4, this.reschedule(doc)];
                     case 10:
-                        _a.sent();
+                        _b.sent();
                         this.processing = false;
-                        _a.label = 11;
+                        _b.label = 11;
                     case 11: return [3, 14];
                     case 12:
-                        err_1 = _a.sent();
+                        err_1 = _b.sent();
                         return [4, this.config.onError.call(this, err_1, this)];
                     case 13:
-                        _a.sent();
+                        _b.sent();
                         return [3, 14];
                     case 14:
                         process.nextTick(function () { return _this.tick(); });
@@ -183,11 +198,11 @@ var MongoCron = (function () {
                             }, {
                                 $set: (_c = {}, _c[this.config.sleepUntilFieldPath] = sleepUntil, _c),
                             }, {
-                                returnOriginal: false,
+                                returnOriginal: true,
                             })];
                     case 1:
                         res = _d.sent();
-                        return [2, res.value];
+                        return [2, [res.value, sleepUntil]];
                 }
             });
         });
@@ -196,7 +211,7 @@ var MongoCron = (function () {
         if (!dot.pick(this.config.intervalFieldPath, doc)) {
             return null;
         }
-        var start = moment(dot.pick(this.config.sleepUntilFieldPath, doc)).subtract(this.config.lockDuration, 'millisecond');
+        var start = moment(dot.pick(this.config.sleepUntilFieldPath, doc));
         var future = moment().add(this.config.reprocessDelay, 'millisecond');
         if (start >= future) {
             return start.toDate();
@@ -204,7 +219,10 @@ var MongoCron = (function () {
         try {
             var schedule = later.parse.cron(dot.pick(this.config.intervalFieldPath, doc), true);
             var dates = later.schedule(schedule).next(2, future.toDate(), dot.pick(this.config.repeatUntilFieldPath, doc));
-            var next = dates[1];
+            var next = dates[0];
+            if (this.config.reprocessDelay === 0 && future.isSame(next)) {
+                next = dates.length > 0 ? dates[1] : null;
+            }
             return next instanceof Date ? next : null;
         }
         catch (err) {
@@ -244,6 +262,21 @@ var MongoCron = (function () {
                         _c.label = 6;
                     case 6: return [2];
                 }
+            });
+        });
+    };
+    MongoCron.prototype.saveStatistics = function (doc, jobStart, jobEnd) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                this.config.statisticsCollection.insert({
+                    document: doc,
+                    jobStart: jobStart,
+                    jobEnd: jobEnd,
+                    executionTime: jobEnd.getTime() - jobStart.getTime(),
+                    serverName: this.serverName,
+                    cronName: this.config.cronName
+                });
+                return [2];
             });
         });
     };
--- src/tests/cron.test.ts
+++ src/tests/cron.test.ts
@@ -8,7 +8,9 @@ test.beforeEach(async (t) => {
   t.context.mongo = await MongoClient.connect('mongodb://localhost:27017');
   t.context.db = t.context.mongo.db('test');
   t.context.collection = t.context.db.collection('jobs');
+  t.context.statisticsCollection = t.context.db.collection('cronStats');
   try { await t.context.collection.drop(); } catch (e) {}
+  try { await t.context.statisticsCollection.drop(); } catch (e) {}
 });
 
 test.afterEach(async (t) => {
@@ -17,7 +19,8 @@ test.afterEach(async (t) => {
 
 test.serial('document with `sleepUntil` should be processed', async (t) => {
   let c = new MongoCron({
-    collection: t.context.collection
+    collection: t.context.collection,
+    onDocument: function() {}
   });
   await t.context.collection.insert([
     {sleepUntil: null},
@@ -56,7 +59,8 @@ test.serial('cron should trigger the `onIdle` handler only once', async (t) => {
   let count = 0;
   let c = new MongoCron({
     collection: t.context.collection,
-    onIdle: () => count++
+    onIdle: () => count++,
+    onDocument: function() {}
   });
   await c.start();
   await sleep(1000);
@@ -137,6 +141,7 @@ test.serial('document should stop recurring at `repeatUntil`', async (t) => {
   let stop = moment().add(3000, 'millisecond');
   let repeated = 0;
   let c = new MongoCron({
+    statisticsCollection: t.context.statisticsCollection,
     collection: t.context.collection,
     onDocument: async (doc) => repeated++,
     reprocessDelay: 1000
@@ -149,12 +154,15 @@ test.serial('document should stop recurring at `repeatUntil`', async (t) => {
   });
   await sleep(3000);
   await c.stop();
-  t.is(repeated, 2);
+  // It will run 3 times now because of the changes made on the next scheduling
+  // date. Before it was skipping the first reschedule
+  t.is(repeated, 3);
 });
 
 test.serial('document with `autoRemove` should be deleted when completed', async (t) => {
   let c = new MongoCron({
-    collection: t.context.collection
+    collection: t.context.collection,
+    onDocument: function() {}
   });
   await c.start();
   await t.context.collection.insert({
@@ -164,4 +172,203 @@ test.serial('document with `autoRemove` should be deleted when completed', async
   await sleep(2000);
   await c.stop();
   t.is(await t.context.collection.count(), 0);
+});
+
+test.serial('documents with `rescheduleIfSleepUntilIsNull` should be rescheduled and not run immediatly', async (t) => {
+  let repeated = 0;
+  let c = new MongoCron({
+    collection: t.context.collection,
+    onDocument: async (doc) => repeated++,
+    reprocessDelay: 1000,
+    rescheduleIfSleepUntilIsNull: true
+  });
+  const now = moment.utc();
+  await c.start();
+  await t.context.collection.insertMany([{
+      name: 'test-month',
+      sleepUntil: null,
+      interval: now.clone().add(1, 'month').format('s m H D M *')
+    }, {
+      name: 'test-hour',
+      sleepUntil: null,
+      interval: now.clone().add(1, 'hour').format('s m H * * *')
+    }, {
+      name: 'test-day',
+      sleepUntil: null,
+      interval: now.clone().add(1, 'day').format('s m H D * *')
+    }, {
+      name: 'past-hour',
+      sleepUntil: null,
+      interval: now.clone().subtract(1, 'hour').format('s m H * * *')
+  }]);
+  await sleep(1000);
+  await c.stop();
+  now.milliseconds(0);
+  let testMonth = await t.context.collection.findOne({name: 'test-month'});
+  t.is(testMonth.sleepUntil.toISOString(), now.clone().add(1, 'month').toISOString());
+  let testHour = await t.context.collection.findOne({name: 'test-hour'});
+  t.is(testHour.sleepUntil.toISOString(), now.clone().add(1, 'hour').toISOString());
+  let testDay = await t.context.collection.findOne({name: 'test-day'});
+  t.is(testDay.sleepUntil.toISOString(), now.clone().add(1, 'day').toISOString());
+  let pastHour = await t.context.collection.findOne({name: 'past-hour'});
+  t.is(pastHour.sleepUntil.toISOString(), now.clone().subtract(1, 'hour').add(1, 'day').toISOString());
+  // await new Promise((resolve) => {
+  //   t.context.collection.find({}).toArray((err, arr) => {
+  //     console.log(arr);
+  //     resolve();
+  //   });
+  // });
+
+  t.is(repeated, 0);
+});
+
+test.serial('reschedule to the next day', async (t) => {
+  let repeated = 0;
+  let c = new MongoCron({
+    collection: t.context.collection,
+    lockDuration: 5000,
+    onDocument: async (doc) => repeated++,
+    rescheduleIfSleepUntilIsNull: true
+  });
+  let now = moment.utc();
+  let hr = now.hour();
+  let mi = now.minute();
+  await sleep(1000);
+  await c.start();
+  await t.context.collection.insert({
+    sleepUntil: null,
+    interval: `0 ${mi} ${hr} * * *`
+  });
+  await sleep(1000);
+  await c.stop();
+  let res = await t.context.collection.findOne({});
+  let sleepUntil = moment(res.sleepUntil);
+  let now2 = now.clone();
+  now2.seconds(0);
+  now2.millisecond(0);
+  now2.add(1, 'day');
+  sleepUntil.millisecond(0);
+  t.is(repeated, 0);
+  t.is(sleepUntil.toISOString(), now2.toISOString());
+});
+
+test.serial('reschedule to the next hour', async (t) => {
+  let repeated = 0;
+  let c = new MongoCron({
+    collection: t.context.collection,
+    onDocument: async (doc) => repeated++,
+    rescheduleIfSleepUntilIsNull: true
+  });
+  let now = moment.utc();
+  now.seconds(0);
+  now.subtract(1, 'minute');
+  let mi = now.minute();
+  await c.start();
+  await t.context.collection.insert({
+    sleepUntil: null,
+    interval: `0 ${mi} * * * *`
+  });
+  await sleep(1000);
+  await c.stop();
+  let res = await t.context.collection.findOne({});
+  let sleepUntil = moment(res.sleepUntil);
+  let now2 = now.clone();
+  now2.seconds(0);
+  now2.millisecond(0);
+  now2.add(1, 'hour');
+  sleepUntil.millisecond(0);
+  t.is(repeated, 0);
+  t.is(sleepUntil.toISOString(), now2.toISOString());
+});
+
+test.serial('start and reschedule correctly', async (t) => {
+  let repeated = 0;
+  let c = new MongoCron({
+    collection: t.context.collection,
+    onDocument: async (doc) => repeated++,
+    nextDelay: 100
+  });
+  let now = moment.utc();
+  now.add(2, 'second');
+  await c.start();
+  await t.context.collection.insert({
+    sleepUntil: now.toDate(),
+    interval: now.format('s m H D M *')
+  });
+  await sleep(3000);
+  await c.stop();
+  let res = await t.context.collection.findOne({});
+  let sleepUntil = moment(res.sleepUntil);
+  let now2 = now.clone();
+  now2.millisecond(0);
+  now2.add(1, 'year');
+  sleepUntil.millisecond(0);
+  t.is(repeated, 1);
+  t.is(sleepUntil.toISOString(), now2.toISOString());
+});
+
+test.serial('return document with sleepUntil in the future', async (t) => {
+  let sleepUntil = null;
+  let c = new MongoCron({
+    collection: t.context.collection,
+    onDocument: async (doc) => { sleepUntil = doc.sleepUntil; }
+  });
+  await c.start();
+  await t.context.collection.insert({
+    sleepUntil: moment().subtract(1, 'hour').toDate(),
+    interval: '* * * * * *'
+  });
+  await sleep(1000);
+  await c.stop();
+  t.false(sleepUntil == null);
+  t.true(moment().isBefore(sleepUntil));
+});
+
+test.serial('return document with the original sleepUntil', async (t) => {
+  let sleepUntil = null;
+  let c = new MongoCron({
+    collection: t.context.collection,
+    onDocument: async (doc) => { sleepUntil = doc.sleepUntil; },
+    returnOriginalDocument: true
+  });
+  await c.start();
+  await t.context.collection.insert({
+    sleepUntil: moment().subtract(1, 'hour').toDate(),
+    interval: '* * * * * *'
+  });
+  await sleep(1000);
+  await c.stop();
+  t.false(sleepUntil == null);
+  t.true(moment().isAfter(sleepUntil));
+});
+
+test.serial('should save statistics', async (t) => {
+  let c = new MongoCron({
+    collection: t.context.collection,
+    statisticsCollection: t.context.statisticsCollection,
+    onDocument: async (doc) => { await sleep(doc.sleepMs); },
+    cronName: 'my beautiful cron'
+  });
+  await c.start();
+  await t.context.collection.insertMany([{
+      sleepUntil: null,
+      interval: '* * * * * *',
+      sleepMs: 200
+    }, {
+      sleepUntil: null,
+      interval: moment().add(1, 'second').format('s m H * * *'),
+      sleepMs: 1000
+  }]);
+  await sleep(3000);
+  await c.stop();
+  await new Promise((resolve) => {
+    t.context.statisticsCollection.find({}).toArray((err, arr) => {
+      // console.log(arr);
+      t.is(arr.length, 4);
+      t.is(arr[0].cronName, 'my beautiful cron');
+      t.truthy(arr[0].executionTime);
+      t.truthy(arr[0].serverName);
+      resolve();
+    });
+  });
 });
